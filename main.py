e59af4fe41c45a0c4dd0ca7d66c09ac3c4ce6910
Egils Kalns 191RBC023 <125309153+egilskalns@users.noreply.github.com>
2023-03-24 12:50:17 +0000

    1

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 class Query:
     def __init__(self, query):
         self.type = query[0]
-        self.number = int(query[1])
+        if self.type == 'call':
+            self.name = query[1]
+        else:
+            self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
 
@@ -17,31 +20,32 @@ def write_responses(result):
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    numbers = {}
+    names = {}
     for cur_query in queries:
         if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            numbers[cur_query.number] = cur_query.name
+            names[cur_query.name] = cur_query.number
+
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
+            if cur_query.number in numbers:
+                name = numbers[cur_query.number]
+                names.pop(name)
+                numbers.pop(cur_query.number)
+        elif cur_query.type == 'find':
             response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            if cur_query.number in numbers:
+                response = numbers[cur_query.number]
             result.append(response)
+        #Piezvanit pec varda
+        elif cur_query.type == 'call':
+            response = 'not found'
+            if cur_query.name in names:
+                response = str(names[cur_query.name])
+            result.append(response)
+        else:
+            print("command not found")
     return result
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
